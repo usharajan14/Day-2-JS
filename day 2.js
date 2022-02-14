248ffe7825ac33418ac63ac484d4371b966b0622
usharajan14 <83410212+usharajan14@users.noreply.github.com>
2022-02-14 11:03:02 +0530

    Submitted assignment

---
+++ day 2.js
@@ -0,0 +1,8 @@
+var a=40;
+var b=50;
+var a1="30";var a2=30;
+console.log(a+b,a-b,a*b,a/b,a%b)
+console.log(a+=20,b-=5,a*=2,a/=2,b%=5)
+console.log(a==b,a!=b,a<b,a>b,a<=b,a>=b)
+console.log(a1===a2,a1!==a2)
+console.log((a1===a2)&&(a1!==a),(b>a1)||(b<a),!(a==a1));
